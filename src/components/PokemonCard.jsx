6e3f3370a01c7d9a3686ed8a0da67f96dd494ec7
SeDev <sebascabalq@gmail.com>
2024-11-06 20:39:53 -0500

    Fix: change styles of pokemon card and add start icon

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,12 @@
 import { Card } from "antd";
 import Meta from "antd/es/card/Meta";
+import { StarOutlined } from '@ant-design/icons';
 
 const PokemonCard = () => {
     return <Card
-        style={{ width: 250 }}
         title="Charmander"
         cover={<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/4.png" alt="Charmander" />}
+        extra={<StarOutlined />}
     >
         <Meta description='fire' />
     </Card>
