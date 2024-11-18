9339a4640cbd3affb3a0dd6c51c114af29002564
SeDev <sebascabalq@gmail.com>
2024-11-18 18:05:23 -0500

    Feat: add logo pokedux and create api connection with axios

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { Col } from 'antd';
 import 'antd/dist/reset.css';
 import Searcher from './components/Searcher';
 import PokemonList from './components/PokemonList';
-
+import Logo from './statics/logo.svg'
 function App() {
   return (
     <div className="App">
+      <Col span={4} offset={10}>
+        <img src={Logo} alt='Pokedux' />
+      </Col>
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
