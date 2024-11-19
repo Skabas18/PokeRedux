acee96ce3318a576dde09ecfe07749f4fc36502c
SeDev <sebascabalq@gmail.com>
2024-11-19 18:32:41 -0500

    Feat: create custom middleware

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import { pokemonsReducer } from './reducers/pokemons';
 import { Provider } from 'react-redux';
-import { legacy_createStore as createStore } from 'redux';
-
+import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux';
+import { featuring, logger } from './middlewares';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const store = createStore(pokemonsReducer);
+const composedEnhancers = compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(logger, featuring));
+const store = createStore(pokemonsReducer, composedEnhancers);
 
 root.render(
   <React.StrictMode>
